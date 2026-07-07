ab0083d75de8c8a1c6598a7261fdee0075d97cf7
Ibrahem218 <Ibrahemprogramming57@gmail.com>
2026-07-07 19:39:29 +0300

    first commit

---
+++ Lessons_1_6/Ibrahem_7_7_26.java
@@ -0,0 +1,18 @@
+package Lessons_1_6;
+
+public class Ibrahem_7_7_26 {
+    static void main() {
+
+
+    }
+    public static int digits(int n){
+        int  k, count=0, r, l;
+        while (n != 0) {
+            count++;
+
+        }
+
+
+        return count;
+    }
+}
